62b1de61b53fd82d52e035a78f823fb8f54a6c67
Kyungseok Oh <bestspx@gmail.com>
2024-12-23 15:24:42 +0900

    #2.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBr97XXqsmmNMA8YzleumPDyZGA016zLSs",
+  authDomain: "owitter-cd253.firebaseapp.com",
+  projectId: "owitter-cd253",
+  storageBucket: "owitter-cd253.firebasestorage.app",
+  messagingSenderId: "309470629682",
+  appId: "1:309470629682:web:28cbb1bc47edf189f7912b",
+  measurementId: "G-34W95L76SD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
