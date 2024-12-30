d445c82ec33c2f55f452b8f0e4ba1c6a7ddeef0c
Kyungseok Oh <bestspx@gmail.com>
2024-12-30 15:47:35 +0900

    #3.3 Protected Routes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,5 @@
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  return (
-    <>
-      <h2>Layout</h2>
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 }
