5faa6ddfe9100378be854be8c88fa5150df50c6b
Kyungseok Oh <bestspx@gmail.com>
2024-12-31 17:50:07 +0900

    #4.3 Uploading Images

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
   display: flex;
@@ -65,6 +66,11 @@ export default function PostTweetForm() {
   const onFileChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const { files } = e.target;
     if (files && files.length === 1) {
+      if (files[0].size >= Math.pow(10, 6)) {
+        alert("Max file size is 1MB.");
+        setFile(null);
+        return;
+      }
       setFile(files[0]);
     }
   };
@@ -75,12 +81,23 @@ export default function PostTweetForm() {
       return;
     try {
       setLoading(true);
-      await addDoc(collection(db, "tweets"), {
+      const docRef = await addDoc(collection(db, "tweets"), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || "Anonymous",
         userId: user.uid,
       });
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${docRef.id}`
+        );
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(docRef, { photo: url });
+      }
+      setTweet("");
+      setFile(null);
     } catch (error) {
       console.log(error);
     } finally {
@@ -90,6 +107,7 @@ export default function PostTweetForm() {
   return (
     <Form onSubmit={onSubmit}>
       <TextArea
+        required
         rows={5}
         maxLength={180}
         onChange={onChange}
