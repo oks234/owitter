7f1d203b5e24a625d61fbbca1e34ff21bb058c9e
Kyungseok Oh <bestspx@gmail.com>
2024-12-31 18:04:58 +0900

    #4.4 Fetching Timeline

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -8,7 +8,6 @@ export default function ProtectedRoute({
   children: React.ReactNode;
 }) {
   const user = auth.currentUser;
-  console.log(user);
   if (user === null) {
     return <Navigate to="/login" />;
   }
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -43,7 +43,6 @@ export default function CreateAccount() {
         email,
         password
       );
-      console.log(credentials.user);
       await updateProfile(credentials.user, { displayName: name });
       navigate("/");
     } catch (e) {
