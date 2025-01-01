ec54fe2d093f1dd20583637b75eaef21b7e7ee2f
Kyungseok Oh <bestspx@gmail.com>
2025-01-01 16:32:35 +0900

    #5.0 User Avatar

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,85 @@
+import styled from "styled-components";
+import { auth, storage } from "../firebase";
+import { useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`;
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+const AvatarInput = styled.input`
+  display: none;
+`;
+const Name = styled.span`
+  font-size: 22px;
+`;
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL);
+  const onAvatarChange: React.ChangeEventHandler<HTMLInputElement> = async (
+    event
+  ) => {
+    const { files } = event.target;
+    if (user === null) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(storage, `avatars/${user.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, { photoURL: avatarUrl });
+    }
+  };
+  return (
+    <Wrapper>
+      <AvatarUpload
+        htmlFor="avatar"
+        style={avatar ? { backgroundColor: "transparent" } : undefined}
+      >
+        {avatar ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        id="avatar"
+        type="file"
+        accept="image/*"
+        onChange={onAvatarChange}
+      />
+      <Name>{user?.displayName ?? "Anonymous"}</Name>
+    </Wrapper>
+  );
 }
