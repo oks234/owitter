ade979f5f8a6d6c9cde0d86a7fa3910d77c51df8
Kyungseok Oh <bestspx@gmail.com>
2025-01-01 16:08:15 +0900

    #4.7 Code Challenge

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,8 +1,15 @@
+import { deleteDoc, deleteField, doc, updateDoc } from "firebase/firestore";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
+import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 import styled from "styled-components";
-import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
-import { deleteObject, ref } from "firebase/storage";
+import { ITweet } from "./timeline";
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,11 +18,13 @@ const Wrapper = styled.div`
   border-radius: 15px;
   gap: 16px;
 `;
-const Column = styled.div``;
+const Column = styled.div`
+  font-size: 0;
+`;
 const Photo = styled.img`
   width: 100px;
   height: 100px;
-  border-radius: 15px;
+  border-radius: 0.5rem;
 `;
 const Username = styled.span`
   font-weight: 600;
@@ -24,10 +33,31 @@ const Username = styled.span`
 const Payload = styled.p`
   margin: 10px 0px;
   font-size: 18px;
+  white-space: pre;
+`;
+const TextArea = styled.textarea`
+  margin: 10px 0px;
+  font-size: 18px;
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
 `;
 
-const DeleteButton = styled.button`
-  background-color: tomato;
+const Button = styled.button`
   color: white;
   font-weight: 600;
   border: 0;
@@ -37,14 +67,64 @@ const DeleteButton = styled.button`
   border-radius: 5px;
   cursor: pointer;
 `;
+const CancelButton = styled(Button)`
+  background-color: orange;
+`;
+const DeleteButton = styled(Button)`
+  background-color: tomato;
+`;
+const EditButton = styled(Button)`
+  background-color: green;
+  margin-right: 0.5rem;
+`;
+const SaveButton = styled(Button)`
+  background-color: blue;
+  margin-right: 0.5rem;
+`;
+
+const EditPhoto = styled.div`
+  margin-top: 0.375rem;
+  display: grid;
+  text-align: center;
+  gap: 0.5rem;
+`;
+const EditPhotoLabel = styled(Button)`
+  /* display: block; */
+  /* position: absolute;
+  right: 0.25rem;
+  top: 0.25rem;
+  padding: 0.375rem;
+  display: flex;
+  align-items: center;
+  justify-content: center; */
+  background-color: green;
+
+  svg {
+    width: 1rem;
+  }
+`;
+
+const EditPhotoInput = styled.input`
+  display: none;
+`;
+
+const ButtonLoadingSpinner = () => (
+  <ClipLoader color="white" size={12} cssOverride={{ marginRight: ".25rem" }} />
+);
 
 export default function Tweet({ id, userId, username, photo, tweet }: ITweet) {
+  const [isLoading, setLoading] = useState(false);
+  const [updatingTweet, setUpdatingTweet] = useState(tweet);
+  const [updatingPhoto, setUpdatingPhoto] = useState<File | null>(null);
+  const [deletingPhoto, setDeletingPhoto] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const user = auth.currentUser;
   const onDelete = async () => {
     if (user?.uid !== userId) return;
     const ok = confirm("Are you sure delete tweet?");
     if (!ok) return;
     try {
+      setLoading(true);
       await deleteDoc(doc(db, "tweets", id));
       if (photo) {
         const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
@@ -53,18 +133,138 @@ export default function Tweet({ id, userId, username, photo, tweet }: ITweet) {
     } catch (error) {
       console.log(error);
     } finally {
+      setLoading(false);
     }
+  };
+  const onEdit = () => {
+    setEditMode(true);
+  };
+  const onCancel = () => {
+    setUpdatingTweet(tweet);
+    setEditMode(false);
+    setUpdatingPhoto(null);
+    setDeletingPhoto(false);
+  };
+  const onSave: React.FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    if (user?.uid !== userId) return;
+    if (updatingTweet === "") return;
+    const ok = confirm("Are you sure update tweet?");
+    if (!ok) return;
+    try {
+      setLoading(true);
+      await updateDoc(doc(db, "tweets", id), { tweet: updatingTweet });
+      if (updatingPhoto) {
+        const locationRef = ref(storage, `tweets/${user.uid}/${id}`);
+        const result = await uploadBytes(locationRef, updatingPhoto);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc(db, "tweets", id), { photo: url });
+      }
+      if (deletingPhoto) {
+        await updateDoc(doc(db, "tweets", id), { photo: deleteField() });
+        const locationRef = ref(storage, `tweets/${user.uid}/${id}`);
+        await deleteObject(locationRef);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setEditMode(false);
+      setLoading(false);
+      setUpdatingPhoto(null);
+      setDeletingPhoto(false);
+    }
+  };
+  const onPhotoChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      if (files[0].size >= Math.pow(10, 6)) {
+        alert("Max file size is 1MB.");
+        setUpdatingPhoto(null);
+        return;
+      }
+      setUpdatingPhoto(files[0]);
+      setDeletingPhoto(false);
+    }
+  };
+  const onDeletePhoto = () => {
+    setUpdatingPhoto(null);
+    setDeletingPhoto(true);
   };
   return (
     <Wrapper>
       <Column style={{ flexGrow: 1 }}>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+        {editMode ? (
+          <form onSubmit={onSave}>
+            <TextArea
+              disabled={isLoading}
+              id="tweet"
+              placeholder="tweet"
+              name="tweet"
+              value={updatingTweet}
+              onChange={(e) => {
+                setUpdatingTweet(e.target.value);
+              }}
+            />
+            <EditPhotoInput
+              id="photo"
+              type="file"
+              accept="image/*"
+              hidden
+              disabled={isLoading}
+              onChange={onPhotoChange}
+            />
+            <SaveButton disabled={isLoading} type="submit">
+              {isLoading ? <ButtonLoadingSpinner /> : null}
+              Save
+            </SaveButton>
+            <CancelButton disabled={isLoading} onClick={onCancel}>
+              Cancel
+            </CancelButton>
+          </form>
+        ) : (
+          <>
+            <Payload>{tweet}</Payload>
+          </>
+        )}
         {user?.uid === userId ? (
-          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+          <>
+            {editMode ? null : (
+              <>
+                <EditButton disabled={isLoading} onClick={onEdit}>
+                  Edit
+                </EditButton>
+                <DeleteButton disabled={isLoading} onClick={onDelete}>
+                  {isLoading ? <ButtonLoadingSpinner /> : null}
+                  Delete
+                </DeleteButton>
+              </>
+            )}
+          </>
         ) : null}
       </Column>
-      <Column>{photo ? <Photo src={photo} /> : null}</Column>
+      <Column>
+        {(photo || updatingPhoto) && !deletingPhoto ? (
+          <Photo
+            src={updatingPhoto ? URL.createObjectURL(updatingPhoto) : photo}
+          />
+        ) : null}
+        {editMode ? (
+          <>
+            {deletingPhoto && <Photo as="div" src="" />}
+            <EditPhoto>
+              <EditPhotoLabel as="label" htmlFor="photo">
+                {deletingPhoto || !(photo || updatingPhoto)
+                  ? "add photo"
+                  : "change"}
+              </EditPhotoLabel>
+              <DeleteButton disabled={isLoading} onClick={onDeletePhoto}>
+                delete
+              </DeleteButton>
+            </EditPhoto>
+          </>
+        ) : null}
+      </Column>
     </Wrapper>
   );
 }
